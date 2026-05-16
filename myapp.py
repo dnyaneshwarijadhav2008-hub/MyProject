9f281e05e640f839100bd1f5e883576ae8011042
Dnyanu <dnyameshwarijadhav2850@gmail.com>
2026-05-16 18:20:22 +0530

    C5-Added new function using while loop

--- myapp.py
+++ myapp.py
@@ -1,6 +1,15 @@
 # Write python function to print the table of given user input
 def table():
   num = int(input("Enter a number : "))
-  for i in range(1,11)
-      print(f"{num} * {i} = {num * i})
+  for i in range(1,11):
+      print(f"{num} * {i} = {num * i}")
 table()
+
+# Rewrite table function while loop
+def table_new():
+  num = int(input("Enter a number : "))
+  i = 1
+  while i <= 10:
+      print(f"{num} * {i} = {num * i}")
+      i += 1
+table_new()
