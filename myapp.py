4b77c85f49b5eec109321755244d616848988d95
dnyaneshwarijadhav2008-hub <dnyaneshwarijadhav2008@gmail.com>
2026-05-16 18:01:51 +0530

    C4 - Added code to myapp.py

---
+++ myapp.py
@@ -0,0 +1,6 @@
+# Write python function to print the table of given user input
+def table():
+  num = int(input("Enter a number : "))
+  for i in range(1,11)
+      print(f"{num} * {i} = {num * i})
+table()
