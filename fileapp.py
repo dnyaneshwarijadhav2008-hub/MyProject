5ed4cb484c611061781f797ade1bcf17dedc4018
Dnyanu <dnyameshwarijadhav2850@gmail.com>
2026-05-17 15:31:38 +0530

    C7-added even number using loop function

---
+++ fileapp.py
@@ -0,0 +1,7 @@
+# Write a function to print even number from 1 to 100 using loop function
+def print_even_numbers():
+    for num in range(1, 101):  # L
+        if num % 2 == 0:       
+            print(num)
+
+print_even_numbers()
